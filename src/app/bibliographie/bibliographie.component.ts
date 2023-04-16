30d733a9919237f125bbce2efd57de776ba3161f
_Potatoes_ <55760169+PotatoesSQL@users.noreply.github.com>
2023-04-16 22:08:36 +0200

    add bibliographie (a finir)
    add bibliographie (a finir)

--- src/app/bibliographie/bibliographie.component.ts
+++ src/app/bibliographie/bibliographie.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./bibliographie.component.css']
 })
 export class BibliographieComponent {
+  showLinks = false;
+  isArrowUp = false;
+  links = [
+    {
+      title: "Best Demonstration of Resonance -MIT professor demonstrates how glass breaks due to forced resonance",
+      url: "https://www.youtube.com/watch?v=pyBGSMxPSG0",
+      class: "youtube",
+      icon: "youtube-icon"
+    },
+    {
+      title: "On n'est pas que des cobayes: Peut-on casser un verre avec la voix ?",
+      url: "https://www.youtube.com/watch?v=DP4wQmOGy5w",
+      class: "youtube",
+      icon: "youtube-icon"
+    },
+    {
+      title: "CQFD : Peut-on casser un verre avec la voix?",
+      url: "https://www.youtube.com/watch?v=e3CW0QTzUlc",
+      class: "youtube",
+      icon: "youtube-icon"
+    },
+    {
+      title: "Science & Vie: Un simple son peut-il réellement casser du verre ?",
+      url: "https://www.science-et-vie.com/article-magazine/un-simple-son-peut-il-reellement-casser-du-verre",
+      class: "article",
+      icon: "article-icon"
+    }
+  ];
 
+  toggleLinks() {
+    this.showLinks = !this.showLinks;
+    this.links.sort((a, b) => a.title.length - b.title.length);
+    this.isArrowUp = !this.isArrowUp;
+  }
 }
