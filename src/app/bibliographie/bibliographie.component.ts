43b8d114f940baf59cd01448729e700dda57a0ea
_Potatoes_ <55760169+PotatoesSQL@users.noreply.github.com>
2023-04-18 22:21:35 +0200

    update icons
    update icons

--- src/app/bibliographie/bibliographie.component.ts
+++ src/app/bibliographie/bibliographie.component.ts
@@ -17,25 +17,25 @@ export class BibliographieComponent {
       title: "Best Demonstration of Resonance -MIT professor demonstrates how glass breaks due to forced resonance",
       url: "https://www.youtube.com/watch?v=pyBGSMxPSG0",
       class: "youtube",
-      icon: "youtube-icon"
+      icon: "bi-youtube"
     },
     {
       title: "On n'est pas que des cobayes: Peut-on casser un verre avec la voix ?",
       url: "https://www.youtube.com/watch?v=DP4wQmOGy5w",
       class: "youtube",
-      icon: "youtube-icon"
+      icon: "bi-youtube"
     },
     {
       title: "CQFD : Peut-on casser un verre avec la voix?",
       url: "https://www.youtube.com/watch?v=e3CW0QTzUlc",
       class: "youtube",
-      icon: "youtube-icon"
+      icon: "bi-youtube"
     },
     {
       title: "Science & Vie: Un simple son peut-il réellement casser du verre ?",
       url: "https://www.science-et-vie.com/article-magazine/un-simple-son-peut-il-reellement-casser-du-verre",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     }
   ];
   linksThermique = [
@@ -43,37 +43,37 @@ export class BibliographieComponent {
       title: "Ultrasonic Cavitation in Water",
       url: "https://www.youtube.com/watch?v=qkYtVjJGNRs",
       class: "youtube",
-      icon: "youtube-icon"
+      icon: "bi-youtube"
     },
     {
       title: "Cordis: Transformer les ondes sonores en vagues de chaleur",
       url: "https://cordis.europa.eu/article/id/86348-turning-sound-waves-into-a-heat-wave/fr",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     },
     {
       title: "La Tribune: Equium invente le chauffage et la climatisation par les ondes sonores",
       url: "https://www.latribune.fr/entreprises-finance/industrie/energie-environnement/equium-invente-le-chauffage-et-la-climatisation-par-les-ondes-sonores-939293.html",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     },
     {
       title: "Begeek: Un son si fort qu’il peut faire bouillir de l’eau instantanément",
       url: "https://www.begeek.fr/un-son-si-fort-quil-peut-faire-bouillir-de-leau-instantanement-341640",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     },
     {
       title: "Linfodurable: Pay-bas : des chercheurs parviennent à générer du froid grâce aux ondes sonores",
       url: "https://www.linfodurable.fr/technomedias/pays-bas-des-chercheurs-parviennent-generer-du-froid-grace-aux-ondes-sonores-9672",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     },
     {
-      title: "Wikipédia: Thermoacoustique",
+      title: "Wikipédia: Thermoacoustique ",
       url: "https://fr.wikipedia.org/wiki/Thermoacoustique",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     }
   ];
   linksElectrique = [
@@ -81,7 +81,7 @@ export class BibliographieComponent {
       title: "LeParisien: Futur : le bruit, nouvelle source d’énergie",
       url: "https://www.leparisien.fr/week-end/futur-le-bruit-nouvelle-source-d-energie-17-06-2013-2904413.php",
       class: "article",
-      icon: "article-icon"
+      icon: "bi-book-fill"
     }
   ]
   toggleLinksMecanique() {
